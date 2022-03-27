7f760b7e1bf850dd1f295fe3c36231346e9b8cdb
Hansanghyeon <hyeon@imweb.me>
2022-03-28 07:08:43 +0900

    feat: storybook 예제 추가

---
+++ src/table/Default/index.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Table from "./index.svelte";
+</script>
+
+<Meta
+  title="Example/Table"
+  component={Table}
+/>
+
+<Template let:args>
+  <Table {...args} />
+</Template>
+
+<Story
+  name="default"
+/>
+
---
+++ src/table/Default/index.svelte
@@ -0,0 +1,26 @@
+<script>
+  let data = {
+    
+  }
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <td>일자</td>
+      <td>지급</td>
+      <td>차감</td>
+      <td>소멸</td>
+      <td>상세 내역</td>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
+
+<style>
+  table {
+    border-radius: 0;
+  }
+</style>
