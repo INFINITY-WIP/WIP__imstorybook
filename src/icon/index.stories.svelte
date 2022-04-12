116f623e565aa7b03c639433c30a7df22589f788
999 <hyeon@imweb.me>
2022-04-12 10:30:49 +0900

    feat: imweb icon font

--- src/icon/index.stories.svelte
+++ src/icon/index.stories.svelte
@@ -7,7 +7,33 @@
 />
 
 <Template let:args>
-  <i class="icon-"></i>
+  <div class="flex flex-wrap gap-[16px] text-[36px]">
+    <i class="ic-imweb"></i>
+    <i class="ic-arrow-left"></i>
+    <i class="ic-bell"></i>
+    <i class="ic-circle-arrow-right"></i>
+    <i class="ic-chains"></i>
+    <i class="ic-circle-delete"></i>
+    <i class="ic-close"></i>
+    <i class="ic-check-solid"></i>
+    <i class="ic-delete"></i>
+    <i class="ic-decrement"></i>
+    <i class="ic-extinction"></i>
+    <i class="ic-equals-plain"></i>
+    <i class="ic-global"></i>
+    <i class="ic-increment"></i>
+    <i class="ic-more"></i>
+    <i class="ic-mappin"></i>
+    <i class="ic-minus-solid"></i>
+    <i class="ic-outline-alert"></i>
+    <i class="ic-plus-solid"></i>
+    <i class="ic-remaining"></i>
+    <i class="ic-settings"></i>
+    <i class="ic-under"></i>
+    <i class="ic-check before:min-w-[1.2em]"></i>
+    <i class="ic-arrow-long-left before:min-w-[1.8em]"></i>
+    <i class="ic-arrow-long-right before:min-w-[1.8em]"></i>
+  </div>
 </Template>
 
 <Story
